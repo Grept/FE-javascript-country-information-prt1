cf94a8cd2084cd9d72664f31a6790f9e4563fa51
Grept <tomjansen.408@gmail.com>
2022-02-14 16:36:55 +0100

    Finished search-page logic. Did some initial styling.

--- src/pages/search-page/search-page.js
+++ src/pages/search-page/search-page.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
+// MAIN FUNCTION
 async function getCountryData(countryName) {
     try {
+        console.log("Inside getCountryData try-block")
+        // GET DATA FROM API
         const {data: [countryData]} = await axios.get(`https://restcountries.com/v2/name/${countryName}`);
+
+        // CALL FUNCTION TO PROCES API-DATA
         displayCountryData(countryData);
+        document.getElementById("search-form").reset();
         console.log(countryData);
     } catch (e) {
         console.log(e);
@@ -11,21 +17,27 @@ async function getCountryData(countryName) {
 }
 
 function displayCountryData(country) {
+    console.log("Inside displayCountryData")
+
     // GET INFO ELEMENT
-    const infoContainer = document.getElementById("infoContainer");
+    const infoContainer = document.getElementById("info-container");
 
     // CLEAR THE DATA ALREADY ON SCREEN
     infoContainer.innerHTML = "";
 
+    // CREATE INFO ELEMENT
     const infoElement = document.createElement("div");
     infoElement.setAttribute("class", "info-element");
     infoElement.innerHTML = createCountryInfoString(country);
 
+    // ADD INFO ELEMENT TO PAGE
     infoContainer.appendChild(infoElement);
 
 }
 
 function createCountryInfoString(country) {
+    console.log("Inside createCountryInfoString")
+
     // GET STRING COMPONENTS
     const {flag} = country;
     const {name: countryName} = country;
@@ -33,22 +45,60 @@ function createCountryInfoString(country) {
     const {subregion} = country;
     const {population} = country;
     const {capital} = country;
-    const {currencies: [{name: currencyName}]} = country;
-    const {languages: [{name : countryLanguage}]} = country;
-
-    // check if country ends with s
+    const currencyString = currencyStringBuilder(country);
+    const languageString = languageStringBuilder(country);
 
     return `
         <div>
             <img src="${flag}" alt="${countryCode}">
-            <h3>${countryName}</h3>
+            <div class="header-and-info">
+                <h3>${countryName}</h3>
+                <p class="country-information">
+                    ${countryName} is situated in ${subregion}. It has a population of <strong>${population.toLocaleString("nl-NL")}</strong> people.
+                    The capital is ${capital} and you can pay with ${currencyString}. They speak ${languageString}.
+                </p>
+            </div>
         </div>
-        <p>
-            ${countryName} is situated in ${subregion}. It has a population of <strong>${population}</strong> people.
-            The capital is ${capital} and you can pay with ${currencyName}'s. They speak ${countryLanguage}.
-        </p>
     `;
 }
 
+function currencyStringBuilder(country) {
+    console.log("Inside Currency block")
+    let currencyString = "";
 
-getCountryData("Italy");
+    for (let i = 0; i < country.currencies.length; i++) {
+        currencyString += country.currencies[i].name;
+        if (i < country.currencies.length - 2) {
+            currencyString += ", ";
+        } else if (i < country.currencies.length - 1) {
+            currencyString += " and ";
+        }
+    }
+
+    return currencyString;
+}
+
+function languageStringBuilder(country) {
+    console.log("Inside Language block")
+    let languageString= "";
+
+    for (let i = 0; i <  country.languages.length; i++) {
+        languageString += country.languages[i].name;
+        if (i < country.languages.length - 2) {
+            languageString += ", ";
+        } else if (i < country.languages.length - 1) {
+            languageString += " and ";
+        }
+    }
+
+    return languageString;
+}
+
+// EVENT LISTENER
+const form = document.getElementById("search-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Event triggered: " + event.timeStamp);
+    const {elements: [{value: searchQuery}]} = form;
+    getCountryData(searchQuery);
+});
